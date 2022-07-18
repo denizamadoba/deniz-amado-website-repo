f88165a1018db75cc29668bd24d5df5a451741ca
DenizAmado <damado@brandeis.edu>
2022-07-18 12:02:50 -0400

    Change font, remove unused lines

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -4,8 +4,7 @@ const Nav = () => {
     return (
         <div className="w-full flex px-16 bg-gray-300 text-gray-800 justify-between b-white border-opacity-5 z-100 sticky">
             <div className="flex text-3xl tracking-widest mt-4">
-                <img src="./deniz-logo.png" className="flex h-24"></img>
-                {/* <span class="absolute z-100 left-7 top-8">Deniz Amado</span> */}
+                <img src="./deniz-logo.png" className="flex h-24" alt="logo"></img>
             </div>
             <div className="flex space-x-12 text-xl py-8">
                 <Link to='/#experience' className="hover:text-red-400 hover:scale-110">Experience</Link>
