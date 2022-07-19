4e32cd04c7faa40bc621f1c481aeb4fa74a27f62
DenizAmado <damado@brandeis.edu>
2022-07-19 12:36:28 -0400

    Round the borders of site logo on mobile

--- src/components/IntroStripe.js
+++ src/components/IntroStripe.js
@@ -2,7 +2,7 @@ const IntroStripe = () => {
     return (
         <div id="intro" className="w-full flex pb-12 md:py-12 md:px-56 md:py-40 bg-gradient-to-b from-gray-300 to-red-100 text-gray-800 justify-center flex-col space-y-12">
              <div className="text-3xl tracking-widest flex mx-auto md:hidden">
-                <img src="./deniz-logo.png" className="flex h-16" alt="logo"></img>
+                <img src="./deniz-logo.png" className="flex h-16 rounded-full" alt="logo"></img>
             </div>
             <div className="flex flex-col md:flex-row items-center space-y-8 md:space-y-0 md:space-x-8">
                 <img src="./deniz-professional-2022.jpg" className="w-48 md:w-96 rounded-lg mx-auto" alt="portrait"></img>
